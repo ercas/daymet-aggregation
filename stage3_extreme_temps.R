4b431b4b9f3cbe277cb12deb9bbc93511ff82f6e
Edgar Castro <edgar_castro@g.harvard.edu>
2022-04-27 02:27:08 -0400

    offload detection of extreme heat / cold days

--- stage3_extreme_temps.R
+++ stage3_extreme_temps.R
@@ -20,7 +20,10 @@
 # Contact: Edgar Castro <edgar_castro@g.harvard.edu>
 
 library(data.table)
+library(pbapply)
 library(unglue)
+
+pboptions(type = "timer")
 
 paths <- Sys.glob("output/aggregated-combined/*/*.csv.gz")
 parts <- as.data.table(unglue_data(
@@ -28,32 +31,26 @@ parts <- as.data.table(unglue_data(
   "output/aggregated-combined/{geography}/{aggregation}_{measure}.csv.gz"
 ))[, path := paths][aggregation == "mean"]
 
+parts <- parts[!grepl("block_group", path)]
+
+PERCENTILES <- 0:100 / 100
+
 for (current_geography in unique(parts$geography)) {
   output_directory <- sprintf("output/extra/%s", current_geography)
   dir.create(output_directory, showWarnings = FALSE, recursive = TRUE)
   
   output_tmax_quantiles <- sprintf("%s/tmax_quantiles.csv.gz", output_directory)
   output_tmin_quantiles <- sprintf("%s/tmin_quantiles.csv.gz", output_directory)
-  output_extreme_temps <- sprintf("%s/extreme_temp_days.csv.gz", output_directory)
-  output_extreme_temp_waves <- sprintf("%s/extreme_temp_wave_days.csv.gz", output_directory)
   
-  if (all(sapply(
-    list(
-      output_tmax_quantiles, output_tmin_quantiles,
-      output_extreme_temps, output_extreme_temp_waves
-    ),
-    file.exists
-  ))) {
-    message(sprintf("Skipping: %s", current_geography))
-  } else if (
-    nrow(parts[geography == current_geography & measure == "tmax"]) > 0 &
-    nrow(parts[geography == current_geography & measure == "tmin"]) > 0
-  ) {
-    message(sprintf("Processing: %s", current_geography))
-    
-    # Read data ----
+  message(sprintf("Processing: %s", current_geography))
   
-    message("( 1/10) Reading and transforming tmax data")
+  if (file.exists(output_tmax_quantiles)) {
+    message("(1/8) Already done")
+    message("(2/8) Already done")
+    message("(3/8) Already done")
+    message("(4/8) Already done")
+  } else {
+    message("(1/8) Reading and transforming tmax data")
     tmax <- fread(parts[geography == current_geography & measure == "tmax"]$path)
     
     # Hacky: do some math to extract the year, month, and day from ISO8601 dates
@@ -67,104 +64,72 @@ for (current_geography in unique(parts$geography)) {
     id_column <- names(tmax)[1]
     setnames(tmax, id_column, "id")
     
-    # Same as above - refer to above for comments
-    message("( 2/10) Reading and transforming tmin data")
-    tmin <- fread(parts[geography == current_geography & measure == "tmax"]$path)
+    # Generate quantiles ----
+    message("(2/8) Calculating tmax percentiles")
+    tmax_agg <- pblapply(
+      PERCENTILES,
+      function(percentile) {
+        result <- tmax[, list(temp = quantile(value, percentile, na.rm = TRUE)),
+                       by = list(id, year)]
+        setnames(result, "temp", sprintf("pctile%02.f", percentile * 100))
+      }
+    )
+    
+    # Merge and write out ----
+    message("(3/8) Merging tmax percentiles")
+    tmax_agg <- Reduce(function(left, right) cbind(left, right[, 3]), tmax_agg)
+    setnames(tmax_agg, "id", id_column)
+    
+    message("(4/8) Writing tmax percentiles")
+    fwrite(tmax_agg, output_tmax_quantiles)
+    
+    rm(tmax)
+    rm(tmax_agg)
+    gc()
+  }
+  
+  # Repeat for tmin
+  if (file.exists(output_tmin_quantiles)) {
+    message("(5/8) Already done")
+    message("(6/8) Already done")
+    message("(7/8) Already done")
+    message("(8/8) Already done")
+  } else {
+    message("(5/8) Reading and transforming tmin data")
+    tmin <- fread(parts[geography == current_geography & measure == "tmin"]$path)
+    
+    # Hacky: do some math to extract the year, month, and day from ISO8601 dates
+    # with no delimiter
     tmin[, year := floor(date / 1e4)]
     tmin[, month := floor((date / 1e4 - year) * 1e2)]
     tmin[, day := date - (floor(date / 1e2) * 1e2)]
-    setnames(tmin, id_column, "id") # Should be same ID column
+    
+    # Guess the ID column (should be the first column) and rename it (syntactically
+    # inconvenient to use variables in data.table `by` argument)
+    id_column <- names(tmin)[1]
+    setnames(tmin, id_column, "id")
     
     # Generate quantiles ----
-    
-    if (file.exists(output_tmax_quantiles)) {
-      message("( 3/10) Skipped")
-      tmax_agg <- fread(output_tmax_quantiles)
-    } else {
-      message("( 3/10) Calculating tmax 1st and 99th percentiles")
-      tmax_agg <- tmax[, .(tmax_pctile01 = quantile(value, 0.01, na.rm = TRUE),
-                           tmax_pctile99 = quantile(value, 0.99, na.rm = TRUE)),
+    message("(6/8) Calculating tmin percentiles")
+    tmin_agg <- pblapply(
+      PERCENTILES,
+      function(percentile) {
+        result <- tmin[, list(temp = quantile(value, percentile, na.rm = TRUE)),
                        by = list(id, year)]
-      setnames(tmax_agg, "id", id_column)
-      fwrite(tmax_agg, output_tmax_quantiles)
-    }
+        setnames(result, "temp", sprintf("pctile%02.f", percentile * 100))
+      }
+    )
     
-    if (file.exists(output_tmin_quantiles)) {
-      message("( 4/10) Skipped")
-      tmin_agg <- fread(output_tmin_quantiles)
-    } else {
-      message("( 4/10) Calculating tmin 1st and 99th percentiles")
-      tmin_agg <- tmin[, .(tmin_pctile01 = quantile(value, 0.01, na.rm = TRUE),
-                           tmin_pctile99 = quantile(value, 0.99, na.rm = TRUE)),
-                       by = list(id, year)]
-      setnames(tmin_agg, "id", id_column)
-      fwrite(tmin_agg, output_tmin_quantiles)
-    }
+    # Merge and write out ----
+    message("(7/8) Merging tmin percentiles")
+    tmin_agg <- Reduce(function(left, right) cbind(left, right[, 3]), tmin_agg)
+    setnames(tmin_agg, "id", id_column)
     
-    # Back to "id" for joining later
-    setnames(tmax_agg, id_column, "id")
-    setnames(tmin_agg, id_column, "id")
+    message("(8/8) Writing tmin percentiles")
+    fwrite(tmin_agg, output_tmin_quantiles)
     
-    # Hot and cold weather indicators ----
-    
-    if (file.exists(output_extreme_temps)) {
-      message("( 5/10) Skipped")
-      message("( 6/10) Skipped")
-      message("( 7/10) Skipped")
-    } else {
-      message("( 5/10) Generating extreme cold indicators")
-      cold <- tmax[tmax_agg, on = list(id, year)
-                   ][value <= tmax_pctile01
-                    ][, list(id, year, month, day)
-                     ][, extreme := "cold"
-                      ][order(id, year, month, day)]
-      setnames(cold, "id", id_column)
-      
-      message("( 6/10) Generating extreme heat indicators")
-      hot <- tmin[tmin_agg, on = list(id, year)
-                  ][value >= tmin_pctile99
-                   ][, list(id, year, month, day)
-                    ][, extreme := "hot"
-                     ][order(id, year, month, day)]
-      setnames(hot, "id", id_column)
-      
-      message("( 7/10) Writing extreme weather indicators")
-      fwrite(rbindlist(list(cold, hot)), output_extreme_temps)
-    }
-    
-    # Hot and cold wave and singleton extreme weather day indicators ----
-    
-    if (file.exists(output_extreme_temp_waves)) {
-      message("( 8/10) Skipped")
-      message("( 9/10) Skipped")
-      message("(10/10) Skipped")
-    } else {
-      message("( 8/10) Generating cold wave indicators")
-      cold_waves <- tmax[tmax_agg, on = list(id, year)]
-      cold_waves
-      cold_waves <- tmax[tmax_agg, on = list(id, year)
-                         ][, `:=` (this_day_cold = value <= tmax_pctile01,
-                                   next_day_cold = shift(value, 1) <= tmax_pctile01,
-                                   prev_day_cold = shift(value, -1) <= tmax_pctile01)
-                          ][(this_day_cold == TRUE & next_day_cold == TRUE) | (this_day_cold == TRUE & prev_day_cold == TRUE)
-                           ][, list(id, year, month, day)
-                            ][, extreme_wave := "cold"
-                             ][order(id, year, month, day)]
-      setnames(cold_waves, "id", id_column)
-      
-      message("( 9/10) Generating heat wave indicators")
-      heat_waves <- tmin[tmin_agg, on = list(id, year)
-                         ][, `:=` (this_day_hot = value >= tmin_pctile99,
-                                   next_day_hot = shift(value, 1) >= tmin_pctile99,
-                                   prev_day_hot = shift(value, -1) >= tmin_pctile99)
-                          ][(this_day_hot == TRUE & next_day_hot == TRUE) | (this_day_hot == TRUE & prev_day_hot == TRUE)
-                           ][, list(id, year, month, day)
-                            ][, extreme_wave := "hot"
-                             ][order(id, year, month, day)]
-      setnames(heat_waves, "id", id_column)
-                
-      message("(10/10) Writing heat wave indicators")
-      fwrite(rbindlist(list(cold_waves, heat_waves)), output_extreme_temp_waves)
-    }
+    rm(tmin)
+    rm(tmin_agg)
+    gc()
   }
 }
